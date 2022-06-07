05f1fd065dc68db8dd9c44f3711d1872d4b62a7e
Douglas <douglasrodriguesconectadoifmg@gmail.com>
2022-06-07 12:02:37 -0300

    java

--- cursojava/executavel/PrimeiroClasseJava.java
+++ cursojava/executavel/PrimeiroClasseJava.java
@@ -1,9 +1,23 @@
 package cursojava.executavel;
 
+import cursojava.classe.Aluno;
+
 public class PrimeiroClasseJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		Aluno aluno1 = new Aluno();
+		Aluno aluno2 = new Aluno();
+		Aluno aluno3 = new Aluno();
+		
+		Aluno aluno4 = new Aluno("Maria");
+		
+		Aluno aluno5 = new Aluno("Maria", 50);
+
+
+		
 
 	}
 
